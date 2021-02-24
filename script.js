74aba51d0fc835ff422b035c3ff109c6812c9d30
VishalVParikh <vishalparikh303@gmail.com>
2021-02-24 05:24:46 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
+function updateSlider(slideAmount)
 {
-    p = document.getElementById("principal").value;
-    
+var sliderDiv = document.getElementById("result");
+sliderDiv.innerHTML = slideAmount;
 }
-        
+
+function compute(){
+event.preventDefault();   
+  var principal = parseFloat(document.getElementById("principal").value);
+
+if(principal<=0 || principal<=0)
+	{
+        alert("enter a positive number in amount");
+        document.getElementById("principal").focus();
+        }
+else
+{
+  var d = new Date();
+  var interestRate1 = parseFloat(document.getElementById("rate").value);
+  interestRate = interestRate1 / 100;
+  var termOfLoan = parseFloat(document.getElementById("years").value);
+  var simpleInt = principal * interestRate * termOfLoan;
+  var amount = principal + simpleInt;
+  var years = d.getFullYear() + termOfLoan;
+  document.getElementById("s1").innerHTML = "If you deposit <mark>" + principal+"</mark>"+",";
+  document.getElementById("s2").innerHTML = "at an interest rate of <mark>" + interestRate1 + "%"+"</mark>"+".";
+  document.getElementById("s3").innerHTML = "You will receive an amount of <mark>" + amount+"</mark>"+",";
+  document.getElementById("s4").innerHTML = "in the year  <mark>" + years+"</mark>";
+
+}
+}
+      
